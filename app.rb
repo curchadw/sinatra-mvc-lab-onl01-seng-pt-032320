2de28720a8240cb06174a2e7568a0b5d003e095e
Curtis Chadwell <laurencechadwell@gmail.com>
2020-06-01 17:34:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
   
   post '/piglatinize' do
     pl = PigLatinizer.new
-    @piglatin = pl.piglatinize(params[:user_input])
+    @piglatin = pl.piglatinize(params["user_phrase"])
     erb :results
   end
   
